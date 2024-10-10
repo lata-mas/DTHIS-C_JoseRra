09d35d9ff4f6233fa2fd0630dc0e31e01a8c1937
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-10 00:43:55 -0600

    Update HDR.sh

--- Scripts/Illuminance/HDR.sh
+++ Scripts/Illuminance/HDR.sh
@@ -3,7 +3,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M")  
 
 # Set the output directory for images
-IMAGE_DIR=/home/hdeza/Illuminance/Images
+IMAGE_DIR=/home/hdeza/Illuminance/images
 
 # Set the output directory for other files
 OUTPUT_DIR=/home/hdeza/Illuminance
@@ -11,10 +11,10 @@ OUTPUT_DIR=/home/hdeza/Illuminance
 # Define the shutter speeds
 SHUTTER_SPEEDS=(5000 10000 50000 100000)
 
-# Take photos with different shutter speeds and save in .dng format
+# Take photos with different shutter speeds and save in .jpg format
 for i in "${!SHUTTER_SPEEDS[@]}"; do
-  rpicam-still -n --gain 1 -t 500 --shutter "${SHUTTER_SPEEDS[$i]}" -r -o "$IMAGE_DIR/$(printf "%02d" $((i+1))).jpg"
+  rpicam-still -n --gain 1 -t 500 --shutter "${SHUTTER_SPEEDS[$i]}" -o "$IMAGE_DIR/$(printf "%02d" $((i+1))).jpg"
 done
 
 # Run raw2hdr with the path to the images
-raw2hdr -a -e -g -f -h -w -o "/home/hdeza/Illuminance/HDR_images/${DATE}.hdr" "$IMAGE_DIR"/*.dng
+# raw2hdr -a -e -g -f -h -w -o "/home/hdeza/Illuminance/HDR_images/${DATE}.hdr" "$IMAGE_DIR"/*.dng
