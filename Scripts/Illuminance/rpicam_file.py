ba9108f2f3f0bc818f1bd9805c633944860ae741
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-11 14:48:37 -0600

    Update rpicam_file.py

--- Scripts/Illuminance/rpicam_file.py
+++ Scripts/Illuminance/rpicam_file.py
@@ -3,7 +3,7 @@ from pidng.camdefs import *
 import numpy as np
 
 # load raw image data from file into numpy array. RAW frame from HQ camera. 
-img = "hq_camera_frame.raw"
+img = "/home/hdeza/Illuminance/RAW_images/02.raw"
 data = np.fromfile(img, dtype=np.uint8)
 
 # choose a predefined camera model, set the sensor mode and bayer layout. 
@@ -12,8 +12,8 @@ camera = RaspberryPiHqCamera(1, CFAPattern.BGGR)
 
 # example of adding custom DNG tags to predefined tags from camera model
 camera.tags.set(Tag.ApertureValue, [[4,1]])             # F 4.0
-camera.tags.set(Tag.ExposureTime, [[1,400]])             # SHUTTER 1/400
-camera.tags.set(Tag.PhotographicSensitivity, [400])     # ISO 400
+camera.tags.set(Tag.ExposureTime, [[1,100]])             # SHUTTER 1/100
+camera.tags.set(Tag.PhotographicSensitivity, [100])     # ISO 100
 
 # example of passing a filter over the rawframe before it is saved. This will simply print the dimensions of the raw image. 
 def print_dimensions(rawFrame):
