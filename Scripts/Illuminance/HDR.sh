232b1972ac3ac43c220268ffb149e2c07934c633
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-16 11:09:33 -0600

    Update HDR.sh

--- Scripts/Illuminance/HDR.sh
+++ Scripts/Illuminance/HDR.sh
@@ -13,8 +13,13 @@ SHUTTER_SPEEDS=(5000 10000 50000 100000)
 
 # Take photos with different shutter speeds and save in .jpg format
 for i in "${!SHUTTER_SPEEDS[@]}"; do
-  rpicam-still -n --gain 1 -t 500 --shutter "${SHUTTER_SPEEDS[$i]}" -o "$IMAGE_DIR/$(printf "%02d" $((i+1))).raw"
+  rpicam-still -n --gain 1 -t 500 --shutter "${SHUTTER_SPEEDS[$i]}" -o "$IMAGE_DIR/$(printf "%02d" $((i+1))).jpg"
 done
+
+python3 Illuminance/PyDNG/examples/utility.py Illuminance/images/01.jpg
+python3 Illuminance/PyDNG/examples/utility.py Illuminance/images/02.jpg
+python3 Illuminance/PyDNG/examples/utility.py Illuminance/images/03.jpg
+python3 Illuminance/PyDNG/examples/utility.py Illuminance/images/04.jpg
 
 # Run raw2hdr with the path to the images
 # raw2hdr -a -e -g -f -h -w -o "/home/hdeza/Illuminance/HDR_images/${DATE}.hdr" "$IMAGE_DIR"/*.dng
