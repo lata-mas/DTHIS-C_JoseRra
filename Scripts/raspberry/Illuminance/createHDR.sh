ff5af0dc38060e82073c21db7013f6531f3a5bd9
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-17 13:40:00 -0600

    Update createHDR.sh

--- Scripts/raspberry/Illuminance/createHDR.sh
+++ Scripts/raspberry/Illuminance/createHDR.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DATE=$(date +"%Y-%m-%d_%H%M")
 IMG_DIR="/home/hdeza/Illuminance/images"
 HDR_IMG="/home/hdeza/Illuminance/HDR_images"
 MAPS_DIR="/home/hdeza/Illuminance/maps"
@@ -13,30 +14,30 @@ for i in "${!SHUTTERS[@]}"; do
 done
 
 # Run raw2hdr with the path to the images and save as image.hdr
-raw2hdr -a -e -g -f -h -w -o "$HDR_IMG/image.hdr" "$IMG_DIR"/*dng
+raw2hdr -a -e -g -f -h -w -o "$HDR_IMG/image_$DATE.hdr" "$IMG_DIR"/*dng
 
 # Get info about the final HDR
-getinfo "$HDR_IMG/image.hdr"
+getinfo "$HDR_IMG/image_$DATE.hdr"
 
 # Nullify exposure value and save in OUTPUT_DIR
-ra_xyze -r -o "$HDR_IMG/image.hdr" > "$OUTPUT_DIR/image_nullEV.hdr"
+ra_xyze -r -o "$HDR_IMG/image_$DATE.hdr" > "$OUTPUT_DIR/image_nullEV_$DATE.hdr"
 
 # Resize image and save in OUTPUT_DIR
-pfilt -1 -x 1000 -y 1000 "$OUTPUT_DIR/image_nullEV.hdr" > "$OUTPUT_DIR/image_resize.hdr"
+pfilt -1 -x 1000 -y 1000 "$OUTPUT_DIR/image_nullEV_$DATE.hdr" > "$OUTPUT_DIR/image_resize_$DATE.hdr"
 
 # Make photometric adjustment and save in OUTPUT_DIR
-pcomb -s 1.8 "$OUTPUT_DIR/image_resize.hdr" > "$OUTPUT_DIR/image_photometric.hdr"
+pcomb -s 1.8 "$OUTPUT_DIR/image_resize_$DATE.hdr" > "$OUTPUT_DIR/image_photometric_$DATE.hdr"
 
 # Change view angle for fisheye and save the final image in IMG_DIR
-getinfo -a "VIEW= -vta -vv 121 -vh 121" < "$OUTPUT_DIR/image_photometric.hdr" > "$IMG_DIR/image_final.hdr"
+getinfo -a "VIEW= -vta -vv 121 -vh 121" < "$OUTPUT_DIR/image_photometric_$DATE.hdr" > "$IMG_DIR/image_final_$DATE.hdr"
 
 # Print illuminance value and save result in OUTPUT_DIR
 echo "Total illuminance is: "
-evalglare -V "$IMG_DIR/image_final.hdr"
-evalglare -V "$IMG_DIR/image_final.hdr" > "$OUTPUT_DIR/illuminance.txt"
+evalglare -V "$IMG_DIR/image_final_$DATE.hdr"
+evalglare -V "$IMG_DIR/image_final_$DATE.hdr" > "$OUTPUT_DIR/illuminance_$DATE.txt"
 
 # Move final HDR image to HDR_images
-mv "$IMG_DIR/image_final.hdr" "$HDR_IMG/image_final.hdr"
+mv "$IMG_DIR/image_final_$DATE.hdr" "$HDR_IMG/image_final_$DATE.hdr"
 
-# Make the luminance map
-falsecolor -s 5000 -d 1 -i "$HDR_IMG/image_final.hdr" -log 3 > "$MAPS_DIR/image_map.hdr"
+# Make the luminance map with configurations and save it
+falsecolor -s 5000 -d 1 -i "$HDR_IMG/image_final_$DATE.hdr" --log 3 > "$MAPS_DIR/image_map_$DATE.hdr"
