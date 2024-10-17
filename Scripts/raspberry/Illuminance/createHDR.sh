d720bcaf35151287fe8065efd95a0c56734bab7a
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-16 20:41:38 -0600

    Update createHDR.sh

--- Scripts/raspberry/Illuminance/createHDR.sh
+++ Scripts/raspberry/Illuminance/createHDR.sh
@@ -19,14 +19,12 @@ raw2hdr -a -e -g -f -h -w -o "/home/hdeza/Illuminance/HDR_images/image.hdr" /hom
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H%M")
-
-# Define variables for paths
 IMG_DIR="/home/hdeza/Illuminance/images"
 HDR_IMG="/home/hdeza/Illuminance/HDR_images/$DATE.hdr"
 
+# Loop through shutter speeds and capture images
 SHUTTERS=(1000 2000 4000 8000 16000 32000 64000 128000 256000)
 
-# Loop through shutter speeds and capture images
 for i in "${!SHUTTERS[@]}"; do
   rpicam-still --raw -n --gain 1 -t 500 --shutter "${SHUTTERS[$i]}" -o "$IMG_DIR/$(printf "%02d" $((i+1))).jpg"
 done
