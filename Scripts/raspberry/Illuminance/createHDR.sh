76b71535a120f248f0689e6af2c6d6b81dfca0b5
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-16 19:20:06 -0600

    update

--- Scripts/raspberry/Illuminance/createHDR.sh
+++ Scripts/raspberry/Illuminance/createHDR.sh
@@ -1,50 +1,15 @@
 #!/bin/bash
 
-DATE=$(date +"%Y-%m-%d_%H%M")  
+# Take photos with different shutter speeds and save in .jpg format
+rpicam-still --raw -n --gain 1 -t 500 --shutter 1000 -o /home/hdeza/Illuminance/images/01.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 2000 -o /home/hdeza/Illuminance/images/02.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 4000 -o /home/hdeza/Illuminance/images/03.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 8000 -o /home/hdeza/Illuminance/images/04.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 16000 -o /home/hdeza/Illuminance/images/05.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 32000 -o /home/hdeza/Illuminance/images/06.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 64000 -o /home/hdeza/Illuminance/images/08.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 128000 -o /home/hdeza/Illuminance/images/09.jpg
+rpicam-still --raw -n --gain 1 -t 500 --shutter 256000 -o /home/hdeza/Illuminance/images/10.jpg
 
-# Set the output directory for images
-IMAGE_DIR=/home/hdeza/Illuminance/Images
-
-# Set the output directory for other files
-OUTPUT_DIR=/home/hdeza/Illuminance
-
-# Define the shutter speeds
-SHUTTER_SPEEDS=(100 500 1000 5000 10000 50000 100000 500000 1000000 2000000)
-
-# Take photos with different shutter speeds and save in .dng format
-for i in "${!SHUTTER_SPEEDS[@]}"; do
-  libcamera-still -n -r --gain 1 --awb daylight -t 500 --shutter ${SHUTTER_SPEEDS[$i]} -o "$IMAGE_DIR/$((i+1)).dng"
-done
-
-raw2hdr -a -e -g -f -h -w -o im.hdr ./Illuminance/Images/*.dng
-
-#Get info about the final HDR
-getinfo im.hdr
-
-#Nullificate exposure value
-ra_xyze -r -o im.hdr > image_nullEV.hdr
-
-#Crop image (not cropping for the fisheye of this raspberry) 
-#pcompos -x 2060 -y 2060 image_nullEV.hdr -550 -10 > crop.hdr
-
-#Resize image
-pfilt -1 -x 1000 -y 1000 image_nullEV.hdr > image_resize.hdr
-
-#Make photometric adjustment
-pcomb -s 1.8 image_resize.hdr > image_photometric.hdr
-
-#Change view angle for fisheye
-getinfo -a "VIEW= -vta -vv 121 -vh 121" < image_photometric.hdr > image_final.hdr 
-
-#Print illuminance value
-echo "Total illuminance is: "
-evalglare -V image_final.hdr
-evalglare -V image_final.hdr > illuminance.txt
-
-# Move output files to the directory
-mv image_nullEV.hdr $OUTPUT_DIR/
-mv image_photometric.hdr $OUTPUT_DIR/
-mv image_resize.hdr $OUTPUT_DIR/
-mv illuminance.txt $OUTPUT_DIR/
-
-mv image_final.hdr /home/hdeza/Illuminance/HDR_images/$DATE.hdr
+# Run raw2hdr with the path to the images
+raw2hdr -a -e -g -f -h -w -o "/home/hdeza/Illuminance/HDR_images/image.hdr" /home/hdeza/Illuminance/images/*dng
