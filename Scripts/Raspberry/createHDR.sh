2169c7e91d44043487bfcd9b9d08a640da0d2840
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-10-25 15:34:53 -0600

    Update createHDR.sh

--- Scripts/Raspberry/createHDR.sh
+++ Scripts/Raspberry/createHDR.sh
@@ -7,7 +7,7 @@ MAPS_DIR="/home/hdeza/Luminance/maps"
 OUTPUT_DIR="/home/hdeza/Luminance"
 
 # Loop through shutter speeds and capture images
-SHUTTERS=(1000 2000 4000 8000 16000 32000 64000 128000 256000 512000)
+SHUTTERS=(100 500 1000 5000 10000 50000 100000 500000 1000000 2000000)
 
 for i in "${!SHUTTERS[@]}"; do
   rpicam-still --raw -n --gain 1 -t 500 --shutter "${SHUTTERS[$i]}" -o "$IMG_DIR/$(printf "%02d" $((i+1))).jpg"
@@ -19,19 +19,19 @@ raw2hdr -a -e -g -f -h -w -o "$HDR_IMG/image_$DATE.hdr" "$IMG_DIR"/*dng
 # Get info about the final HDR
 getinfo "$HDR_IMG/image_$DATE.hdr"
 
-# Nullify exposure value and save in OUTPUT_DIR
+# Nullify exposure value
 ra_xyze -r -o "$HDR_IMG/image_$DATE.hdr" > "$OUTPUT_DIR/image_nullEV_$DATE.hdr"
 
-# Resize image and save in OUTPUT_DIR
+# Resize image
 pfilt -1 -x 1000 -y 1000 "$OUTPUT_DIR/image_nullEV_$DATE.hdr" > "$OUTPUT_DIR/image_resize_$DATE.hdr"
 
-# Make photometric adjustment and save in OUTPUT_DIR
+# Make photometric adjustment
 pcomb -s 1.8 "$OUTPUT_DIR/image_resize_$DATE.hdr" > "$OUTPUT_DIR/image_photometric_$DATE.hdr"
 
-# Change view angle for fisheye and save the final image in IMG_DIR (160° horizontal and 120° vertical)
-getinfo -a "VIEW= -vta -vv 120 -vh 160" < "$OUTPUT_DIR/image_photometric_$DATE.hdr" > "$IMG_DIR/image_final_$DATE.hdr"
+# Change view angle for fisheye
+getinfo -a "VIEW= -vta -vv 160 -vh 160" < "$OUTPUT_DIR/image_photometric_$DATE.hdr" > "$IMG_DIR/image_final_$DATE.hdr"
 
-# Print illuminance value and save result in OUTPUT_DIR
+# Print illuminance value
 echo "Total illuminance is: "
 evalglare -V "$IMG_DIR/image_final_$DATE.hdr"
 evalglare -V "$IMG_DIR/image_final_$DATE.hdr" > "$OUTPUT_DIR/illuminance_$DATE.txt"
@@ -40,7 +40,6 @@ evalglare -V "$IMG_DIR/image_final_$DATE.hdr" > "$OUTPUT_DIR/illuminance_$DATE.t
 mv "$IMG_DIR/image_final_$DATE.hdr" "$HDR_IMG/image_final_$DATE.hdr"
 
 # Crop to a 160 degree view
-# pcomb -e 'Cx:500;Cy:500;R:;sq(x):x*x' -e 'inC=sq(R)-sq(x-Cx)-sq(y-Cy)' -e 'ro=if(inC,ri(1),0);go=if(inC,gi(1),0);bo=if(inC,bi(1),0)' "$HDR_IMG/image_final_$DATE.hdr" > "$HDR_IMG/image_crop_$DATE.hdr"
 pcomb -e 'Cx:xmax/2;Cy:ymax/2;R:444.44;sq(x):x*x' -e 'inC=sq(R)-sq(x-Cx)-sq(y-Cy)' -e 'ro=if(inC,ri(1),0);go=if(inC,gi(1),0);bo=if(inC,bi(1),0)' "$HDR_IMG/image_final_$DATE.hdr" > "$HDR_IMG/image_crop_$DATE.hdr"
 
 # Make the luminance map with configurations and save it
