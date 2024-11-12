ad9833e8e6f1f59202e0b11a5dabd5548641d082
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2024-11-12 14:41:56 -0600

    Update sound.py

--- Scripts/Raspberry/sound.py
+++ Scripts/Raspberry/sound.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 def sound_data():
     """
-    Función para obtener los datos de sonido desde archivos.
+    Función para obtener los datos de sonido desde archivos y calcular sus valores en dB.
 
     Retorna:
-        dict: Datos de sonido (RMS, dBmax, dBmin).
+        dict: Datos de sonido en dB (RMS_dB, dBmax_dB, dBmin_dB).
     """
     archivos = ['/home/hdeza/Sonido/rms.txt', '/home/hdeza/Sonido/dBmax.txt', '/home/hdeza/Sonido/dBmin.txt']
     rms_x = ""
@@ -19,4 +21,8 @@ def sound_data():
             elif archivo == '/home/hdeza/Sonido/dBmin.txt':
                 dBmin_x = round(float(f.read().strip()), 4)
 
-    return {'RMS': rms_x, 'dBmax': dBmax_x, 'dBmin': dBmin_x}
+    rms_db = round(20 * np.log10(float(rms_x)) + 120, 4)
+    dBmax_db = round(20 * np.log10(float(dBmax_x)) + 120, 4)
+    dBmin_db = round(20 * np.log10(abs(float(dBmin_x))) + 120, 4)
+
+    return {'RMS': rms_db, 'dBmax': dBmax_db, 'dBmin': dBmin_db}
