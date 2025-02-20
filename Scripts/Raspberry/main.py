44a9db8edd7a385fc16cf53eca3171728eaae100
José Ramón Hernández Aguilar <78838898+JRamonHA@users.noreply.github.com>
2025-02-20 17:24:02 -0600

    Update main.py

--- Scripts/Raspberry/main.py
+++ Scripts/Raspberry/main.py
@@ -8,11 +8,6 @@ from sound import sound_data
 # Obtiene los datos de conexión para ThingsBoard
 unique_id, token, thingsboard_host = thingsboard_data()
 
-# Diccionario para almacenar las lecturas del sensor
-sensor_data = {'CO2': 0, 'T': 0, 'HR': 0}
-# Diccionario para datos de sonido
-micro_data = {'RMS': 0, 'dBmax': 0, 'dBmin': 0}  
-
 # Configura el cliente MQTT
 client = mqtt.Client(unique_id, False)
 client.username_pw_set(token, password=None)
@@ -22,23 +17,16 @@ client.loop_start()
 try:
     while True:
         # Obtiene las mediciones del sensor SCD30
-        CO2, T, HR = scd30_measurements()
-
-        # Actualiza el diccionario con las lecturas actuales
-        sensor_data['CO2'] = CO2
-        sensor_data['T'] = T
-        sensor_data['HR'] = HR
-
+        sensor_data = scd30_measurements()
         # Publica los datos del SCD30 en ThingsBoard
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
 
         # Obtiene los datos de sonido
-        micro_data = sound_data() 
-
+        micro_data = sound_data()
         # Publica los datos de sonido en ThingsBoard
         client.publish('v1/devices/me/telemetry', json.dumps(micro_data), 1)
 
-        time.sleep(2)  # Ajusta el intervalo de tiempo entre lecturas
+        time.sleep(2)  # Intervalo de tiempo entre lecturas
 
 except KeyboardInterrupt:
     pass
